8c5ead025fa156a61f8c16fa83d1f728ead6e9df
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2022-01-03 21:07:09 +0530

    WordPress user ID updated in laravel for future reference

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -2,21 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\Customers\CustomerRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class WordpressController extends Controller
 {
-    public function createUser(Request $request)
+    public function createUser(Request $request, CustomerRepository $repo)
     {
         $siteUrl = env('SITE_URL');
 		
+        // These data can be validated through seperate request   
         $request->request->add([
             'username' => env( 'SITE_USERNAME' ),
             'password' => env( 'SITE_PASSWORD' )
         ]);
 
         $response = Http::post("{$siteUrl}/wp-json/el/v1/user", $request->all());
+
+         // Update wordpress user id in the database on success
+         $responseData = json_decode($response->body());
+         if ( !empty($responseData->data) && !empty($responseData->data->wp_user_id) ) :
+             $wpUserId = $responseData->data->wp_user_id;
+         
+             $repo->updateWordpressUserId($request, $wpUserId);
+         endif;
 
         return $response->body(); 
     }
--- app/Repositories/Customers/CustomerRepository.php
+++ app/Repositories/Customers/CustomerRepository.php
@@ -3,11 +3,21 @@
 namespace App\Repositories\Customers;
 
 use App\Models\Customer;
+use Illuminate\Http\Request;
 
 class CustomerRepository
 {
     public function getCustomers()
     {
         return Customer::paginate(10);
+    }
+
+    public function updateWordpressUserId(Request $request, $wpUserId)
+    {
+        $contact = Customer::find($request->id);
+
+        $contact->wp_user_id = $wpUserId;
+
+        $contact->save();
     }
 }
