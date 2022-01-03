1b79d4e629e4874da982b9618c4652577900e6ba
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2022-01-03 18:52:03 +0530

    Admin can view customer profile test case added

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Customer;
 use App\Models\User;
 use Database\Seeders\UserSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -28,6 +29,15 @@ class DashboardTest extends TestCase
     public function test_admin_can_view_dashboard_after_login()
     {
         $response = $this->get(route('dashboard'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_admin_can_view_customer_profile()
+    {
+        $customer = Customer::factory()->create();
+
+        $response = $this->get(route('customer.show', $customer->id));
 
         $response->assertStatus(200);
     }
