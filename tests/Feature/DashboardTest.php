4b5cae333c29564f91222d1adcc4fa1e98cfd9e1
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2022-01-03 18:03:51 +0530

    Admin can view dashboard test case added

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -2,20 +2,27 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DashboardTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+
+    public function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
+
+        // Create a user & authenticate to make request
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+    }
+
+    public function test_admin_can_view_dashboard_after_login()
+    {
+        $response = $this->get(route('dashboard'));
 
         $response->assertStatus(200);
     }
