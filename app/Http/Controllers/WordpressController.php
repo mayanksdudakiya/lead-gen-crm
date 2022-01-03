5996194a0b02e026c49a07ce6c3f017ed8ca7cdc
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2022-01-03 20:58:40 +0530

    WordPress API call mechanism added

--- app/Http/Controllers/WordpressController.php
+++ app/Http/Controllers/WordpressController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class WordpressController extends Controller
 {
-    //
+    public function createUser(Request $request)
+    {
+        $siteUrl = env('SITE_URL');
+		
+        $request->request->add([
+            'username' => env( 'SITE_USERNAME' ),
+            'password' => env( 'SITE_PASSWORD' )
+        ]);
+
+        $response = Http::post("{$siteUrl}/wp-json/el/v1/user", $request->all());
+
+        return $response->body(); 
+    }
 }
