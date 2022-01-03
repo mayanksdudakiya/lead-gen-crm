20dbef4664abf4174b4fe553fb640d7eb7c85a91
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2022-01-03 20:50:24 +0530

    API name parameter required test case added

--- tests/Feature/WordpressUserCreationTest.php
+++ tests/Feature/WordpressUserCreationTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class WordpressUserCreationTest extends TestCase
@@ -52,5 +53,25 @@ class WordpressUserCreationTest extends TestCase
                    $request['name'] == 'John Doe' &&
                    $request['message'] == 'This is the test message';
         });
+    }
+
+    /**
+     * Make a real request to test the parameter validation
+     */
+    public function test_a_name_is_required_parameter()
+    {
+        $response = $this->post(route('create.wordpress.user'), [
+            //'name' => 'John Doe',
+            'phone_number' => '0123456789',
+            'email_address' => 'test@example.com',
+            'budget' => 45675,
+            'message' => 'This is the test message',
+        ]);
+
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->where('code', 'rest_missing_callback_param')
+                 ->where('message', 'Missing parameter(s): name')
+                 ->etc()
+        );
     }
 }
