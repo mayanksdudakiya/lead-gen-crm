3a21d339e0b71d479f38775adaeb133f95ebeec8
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2022-01-03 18:05:20 +0530

    To use refresh database trait user seeder added for admin

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -9,11 +9,16 @@ use Tests\TestCase;
 
 class DashboardTest extends TestCase
 {
+    use RefreshDatabase;
 
     public function setUp(): void
     {
         parent::setUp();
 
+        $this->seed([
+            UserSeeder::class,
+        ]);
+        
         // Create a user & authenticate to make request
         $user = User::factory()->create();
 
