b93c37151d2dec04973a5ad39f68db1f7c2c1556
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2022-01-03 16:53:51 +0530

    Create method added in CustomerController

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class CustomerController extends Controller
 {
-    //
+    public function create()
+    {
+        return Inertia::render('Customer/Form');
+    }
 }
