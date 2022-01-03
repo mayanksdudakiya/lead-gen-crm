d2edddfc11ad88eb870ec4787d1906b185b1cd5e
Mayank Dudakiya <mayanksdudakiya@gmail.com>
2022-01-03 17:15:56 +0530

    Customer store method added with initial mechanism

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class CustomerController extends Controller
@@ -10,5 +11,18 @@ class CustomerController extends Controller
     public function create()
     {
         return Inertia::render('Customer/Form');
+    }
+
+    public function store(AddCustomerRequest $request)
+    {
+        Customer::create([
+            'name' => $request->name,
+            'phone_number' => $request->phone_number,
+            'email_address' => $request->email_address,
+            'budget' => $request->budget,
+            'message' => $request->message,
+        ]);
+
+        return Redirect::route('customer.get')->with('success', 'Your request has been successfully submitted.');
     }
 }
